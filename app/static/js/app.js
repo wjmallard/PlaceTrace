beb4eab27f9066e907eb4be7c9864aa6abcad584
William Mallard <wjm@llard.net>
2025-11-15 17:12:19 -0800

    Add geohash to Visit panel.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,6 +8,50 @@ function placeTraceApp() {
         spatialFilterCircle: null,
         loading: false,
         visits: [],
+        
+        // Geohash encoding function
+        encodeGeohash(lat, lon, precision = 10) {
+            const base32 = '0123456789bcdefghjkmnpqrstuvwxyz';
+            let idx = 0;
+            let bit = 0;
+            let evenBit = true;
+            let geohash = '';
+            
+            let latMin = -90, latMax = 90;
+            let lonMin = -180, lonMax = 180;
+            
+            while (geohash.length < precision) {
+                if (evenBit) {
+                    const lonMid = (lonMin + lonMax) / 2;
+                    if (lon > lonMid) {
+                        idx = (idx << 1) + 1;
+                        lonMin = lonMid;
+                    } else {
+                        idx = idx << 1;
+                        lonMax = lonMid;
+                    }
+                } else {
+                    const latMid = (latMin + latMax) / 2;
+                    if (lat > latMid) {
+                        idx = (idx << 1) + 1;
+                        latMin = latMid;
+                    } else {
+                        idx = idx << 1;
+                        latMax = latMid;
+                    }
+                }
+                evenBit = !evenBit;
+                
+                if (++bit === 5) {
+                    geohash += base32[idx];
+                    bit = 0;
+                    idx = 0;
+                }
+            }
+            
+            return geohash;
+        },
+        
         trips: [],
         activeTripTab: 'day',
         selectedTripId: null,
